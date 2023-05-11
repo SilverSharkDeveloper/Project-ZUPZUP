1958fe90cc7ef9852aee76eac6da8e73de2228f2
publicKJY94 <webdeveloper940301@gmail.com>
2023-05-11 18:49:15 +0900

    게시물

---
+++ src/main/resources/static/js/product/main.js
@@ -0,0 +1,113 @@
+/*
+<script>(function () {
+    if (!(!/(^|;)ohsTopBannerClosed=yes($|;)/.test(document.cookie))) {
+        var style = document.createElement('style');
+        style.append('.gnb-pc-top-banner--unsure { display: none !important; }');
+        document.head.appendChild(style);
+    }
+})();</script>
+<div class=" css-1mvkryr"><a aria-label="PC 최상단 배너" class="css-qaya0j" href="/exhibitions/10162"></a><a
+aria-label="PC 최상단 배너" class="css-1y1zqjo" href="/exhibitions/10162"></a>
+    <div class="css-1rgilik"><a aria-label="PC 최상단 배너" class="css-1duw8h1" href="/exhibitions/10162"></a>
+    </div>
+    <button type="button" aria-label="닫기" class="css-1tt9azx"><span
+class="_dismiss_thick_24 css-1fqbgll"></span></button>
+</div>
+<script>(function () {
+    if (!(/(^|;)ohsAppDownloadPopupClosed=yes($|;)/.test(document.cookie) && !/(^|;)ohsTopBannerClosed=yes($|;)/.test(document.cookie))) {
+        var style = document.createElement('style');
+        style.append('.gnb-mobile-top-banner--unsure { display: none !important; }');
+        document.head.appendChild(style);
+    }
+})();</script>
+*/
+
+/*
+<script async="true" type="text/javascript"
+src="https://sslwidget.criteo.com/event?a=50838&amp;v=5.15.0&amp;p0=e%3Dvh&amp;p1=e%3Dexd%26ref%3Dhttps%253A%252F%252Fwww.google.com%26site_type%3Dd&amp;p2=e%3Ddis&amp;adce=1&amp;bundle=TVJJVl9MZTBMOTRwcjhLWms3WTNWZUI4Y2NvaVRadDhKd0twbENwSTU5ekRZeEJYdXNieFVhaEg2TjJZZkdHJTJGMEl1djNtZkQ5TVB4UEc2QlMwUGxtYUdGcTBvJTJGaGJ5MTVtNERKajlPOEpNVmJ3R2NCa2ViWFpLck1IRW9nYmM5ejFDM2FFOUZvWEpEV0xUQWd1UlZ5JTJCWiUyRmFRdyUzRCUzRA&amp;tld=ohou.se&amp;fu=https%253A%252F%252Fohou.se%252F&amp;pu=https%253A%252F%252Fwww.google.com%252F&amp;ceid=6329cc8a-f70b-4cdc-9a2c-91b3258756d0&amp;dtycbr=88652"></script>
+*/
+
+/*
+<script type="text/javascript" id="">(function () {
+    var b = "change", c = function (a) {
+        window.dataLayer.push({
+            event: "gtm." + a.type,
+            "gtm.element": a.target,
+            "gtm.elementClasses": a.target.className || "",
+            "gtm.elementId": a.target.id || "",
+            "gtm.elementTarget": a.target.target || "",
+            "gtm.elementUrl": a.target.href || a.target.action || "",
+            "gtm.originalEvent": a
+        })
+    };
+    document.addEventListener ? document.addEventListener(b, c, !1) : document.attachEvent && document.attachEvent("on" + b, c)
+})();</script>
+<script type="text/javascript" id="">window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({originalLocation: document.location.protocol + "//" + document.location.hostname + document.location.pathname + document.location.search});</script>
+*/
+
+/*
+
+<script>
+if (window.wcs != null) {
+    if (!window.wcs_add) window.wcs_add = {};
+    window.wcs_add["wa"] = "s_2e85116be219";
+    if (!_nasa) var _nasa = {};
+    wcs.inflow();
+    wcs_do(_nasa);
+}
+</script>
+
+
+<script type="text/javascript" charset="UTF-8" src="//t1.daumcdn.net/adfit/static/kp.js"></script>
+    <script>
+if (window.kakaoPixel != null) {
+    kakaoPixel('7919701595446099671').pageView();
+}
+</script>
+
+
+<script>
+function handleEnlipleLoad() {
+    var rf = new EN();
+    rf.setData('userid', 'bucketplace');
+    rf.setSSL(true);
+    rf.sendRf();
+    if (typeof handleEnlipleLoadShop === 'function') {
+        handleEnlipleLoadShop();
+    }
+}
+</script>
+<script src="https://cdn.megadata.co.kr/js/en_script/3.5/enliple_min3.5.js" async="async"
+onload="handleEnlipleLoad()"></script>*/
+
+/*
+<script>
+window.wptg_tagscript_vars = window.wptg_tagscript_vars || [];
+window.wptg_tagscript_vars.push(function () {
+    return {
+        wp_hcuid: '',
+        ti: '47350',
+        ty: 'Home',
+        device: navigator.userAgent.match(/ios|iphone|ipad|android/i) ? 'mobile' : 'web',
+    };
+});
+</script>
+<script type="text/javascript" async="async" src="//cdn-aitg.widerplanet.com/js/wp_astg_4.0.js"></script>
+
+    <iframe height="0" width="0" title="Criteo DIS iframe" style="display: none;"></iframe>
+    <iframe height="0" width="0" title="Criteo DIS iframe" style="display: none;"></iframe>
+    <script type="text/javascript" id="">(function (a, c, e, f, d, b) {
+    a.hj = a.hj || function () {
+        (a.hj.q = a.hj.q || []).push(arguments)
+    };
+    a._hjSettings = {hjid: 877658, hjsv: 6};
+    d = c.getElementsByTagName("head")[0];
+    b = c.createElement("script");
+    b.async = 1;
+    b.src = e + a._hjSettings.hjid + f + a._hjSettings.hjsv;
+    d.appendChild(b)
+})(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv\x3d");</script>
+<iframe id="_hjSafeContext_69202457" title="_hjSafeContext" tabindex="-1" aria-hidden="true" src="about:blank"
+style="display: none !important; width: 1px !important; height: 1px !important; opacity: 0 !important; pointer-events: none !important;"></iframe>
+*/
