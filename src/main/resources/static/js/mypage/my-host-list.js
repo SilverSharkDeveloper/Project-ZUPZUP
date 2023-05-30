2b868aa4bf353ef2e38d112bea9144f85e6408fa
koreamax <koreamax012@gmail.com>
2023-05-30 17:46:09 +0900

    qwe

--- src/main/resources/static/js/mypage/my-host-list.js
+++ src/main/resources/static/js/mypage/my-host-list.js
@@ -3,11 +3,12 @@ $(document).ready(function(){
 
 
     let text = "";
-    ploggings.forEach(plogging => {
+    ploggings.forEach((plogging) => {
         text += `
             <div class="col-6 col-lg-4" style="">
                                     <div class="css-prip86 e1lm7yo60"><a class="css-beq49r e1vfbmcc3"
                                                                          href="">
+           <!--<img src="/image/mypage/.png" class="image">-->
        </a>
                                     </div>
                                     <div class="css-1647809 e1e2zisd4"><h2 class="css-z2d3s1 e1e2zisd2">${plogging.ploggingTitle}</h2>
@@ -15,7 +16,7 @@ $(document).ready(function(){
                                                 class="project-feed__item__writer"
                                                 href=""><img
                                                 class="project-feed__item__writer__image" alt=""
-                                                src="">
+                                                src="/image/mypage/location.png">
                                             <span
                                                     class="project-feed__item__writer__name">${plogging.ploggingStartLocation}</span></a>
                                         </address>
@@ -23,13 +24,13 @@ $(document).ready(function(){
                                                 class="project-feed__item__writer"
                                                 href=""><img
                                                 class="project-feed__item__writer__image" alt=""
-                                                src=""><span
+                                                src="/image/mypage/base.png"><span
                                                 class="project-feed__item__writer__name">${plogging.userNickname}</span></a>
                                         </address>
-                                        <div class="css-iadmuc e1e2zisd3">좋아요 16<span class="css-4vnul8 e1e2zisd8">조회 6,542</span>
+                                        <div class="css-iadmuc e1e2zisd3">최대 인원 ${plogging.ploggingMaximumParticipant}<span class="css-4vnul8 e1e2zisd8">조회수 ${plogging.ploggingViewedCount}</span>
                                         </div>
                                         <div class="css-iadmuc e1e2zisd3">
-                                            <span class="css-4vnul9 e1e2zisd8">2023.09.01</span>
+                                            <span class="css-4vnul9 e1e2zisd8">${elapsedTime(plogging.ploggingRegisterDate)}</span>
                                         </div>
                                     </div>
              </div>
